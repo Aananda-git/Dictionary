2afb88ee0476b3b8595ba48703db7cd5f0d87a81
Aananda-git <bhusal.aananda1@gmail.com>
2023-08-29 22:36:35 +0545

    api data fetched

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Search from "./components/Search"
-
+import NavBar from "./components/NavBar"
+import Searchme from "./components/Searchme"
 export default function Home(){
     return( 
-        <div className="flex-col items-center ">
-
-            <div className="w-2/3 h-16 border rounded-3xl mt-32 ml-56 bg-stone-100">
-                <Search />
-                
-            </div>
+        
+        <div>
+            <NavBar />
+            <Searchme />
+            
         </div>
-      
-
     )
 }
